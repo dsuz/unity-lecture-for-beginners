283453c09465abc5699026fbe80a5f4d10ba4c52
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-02-07 03:39:21 +0900

    develop breakout

---
+++ Assets/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody2D m_rb;
+    public Vector2 m_startDir = new Vector2(1f, 1f);
+    public float m_startPower = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        // ボールを動かすために力を加える
+        m_rb.AddForce(m_startDir.normalized * m_startPower, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float m_speed = 10f;
+    Rigidbody2D m_rb;
+    
+    // Use this for initialization
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        m_rb.velocity = Vector2.right * h * m_speed;
+    }
+}
